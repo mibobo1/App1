a703317bf8f463e2189ee2fdfd1d235e94bf4369
xuhaifeng <xuhaifeng925@live.com>
2020-01-08 15:45:10 +0800

    20200108

---
+++ BingWallpaper/PictureInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingWallpaper
+{
+
+    /// <summary>
+    /// 壁纸图片信息类
+    /// </summary>
+    class PictureInfo
+    {
+        //壁纸的热点说明信息
+        public List<string> hotspot { get; set; }
+
+        //壁纸的主题
+        public string imgTitle { get; set; }
+
+        //壁纸图片的地址
+        public Uri imageUri
+    }
+}
