a3444b4bd776eec3197d3d0b26625375ceddaec1
许海峰 <xuhaifeng925@live.com>
2020-03-20 15:02:00 +0800

    20200320

---
+++ BingWallpaper/DayPicturesPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.ServiceModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace BingWallpaper
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class DayPicturesPage : Page
+    {
+        public DayPicturesPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// 进入页面即开始加载网络的壁纸图片和信息
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter != null && e.Parameter is int)
+            {
+                //订阅进度事件的处理程序
+                WallpapersService.Current.GetOneDayWallpapersProgressEvent += OnOneDayWallpaperProgressEvent;
+                //调用壁纸请求服务类来获取壁纸信息
+                WallpapersService.Current.GetOneDayWallpapers((int)e.Parameter);
+            }
+        }
+
+        /// <summary>
+        /// 离开当前的页面则移除订阅的进度事件
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            WallpapersService.Current.GetOneDayWallpapersProgressEvent -= OnOneDayWallpaperProgressEvent;
+            base.OnNavigatedFrom(e);
+        }
+
+        /// <summary>
+        /// 进度事件的处理程序
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private  async void OnOneDayWallpaperProgressEvent(object sender, ProgressEventArgs e)
+        {
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+             {
+                 if (e.IsException)
+                 {
+                     //如果发生异常则显示异常信息
+                     info.Text = "获取图片异常：" + e.ExceptionInfo;
+                 }
+                 else
+                 {
+                     //正常返回设置进度条的值
+                     progress.Value = e.ProgressValue;
+                     //进度完整
+                     if (e.Complete)
+                     {
+                         //把显示进度的面板隐藏
+                         tips.Visibility = Visibility.Collapsed;
+                         //把壁纸信息绑定到列表中
+                         pictureList.ItemsSource = e.Pictures;
+                         Debug.WriteLine("e.Pictures.Count:" + e.Pictures.Count);
+                     }
+                 }
+             });
+        }
+
+        /// <summary>
+        /// 查看按钮的事件处理程序
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void view_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- BingWallpaper/MainPage.xaml.cs
+++ BingWallpaper/MainPage.xaml.cs
@@ -22,39 +22,122 @@ namespace BingWallpaper
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private string TodayPictureUri = "http://appserver.m.bing.net/BackgroundImageService/TodayImageService.svc/GetTodayImage?dateOffset=-0" +
+                "&urlEncodeHeaders=true&osName=windowsPhone&osVersion=8.10&orientation=480x800&deviceName=WP8Device&mkt=zh-CN";
         public MainPage()
         {
             this.InitializeComponent();
+            Window.Current.SizeChanged += Current_SizeChanged;
         }
 
+        /// <summary>
+        /// 进入当前的页面
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            background.UriSource = new Uri(TodayPictureUri);
+        }
+
+        /// <summary>
+        /// 查看今天的壁纸
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void today_Click(object sender, RoutedEventArgs e)
         {
-
+            Frame.Navigate(typeof(DayPicturesPage), 0);
         }
 
+        /// <summary>
+        /// 查看昨天的壁纸
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void yesterday_Click(object sender, RoutedEventArgs e)
         {
-
+            Frame.Navigate(typeof(DayPicturesPage), 1);
         }
 
+        /// <summary>
+        /// 查看两天前的壁纸
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void twodayago_Click(object sender, RoutedEventArgs e)
         {
+            Frame.Navigate(typeof(DayPicturesPage), 2);
 
         }
 
+        /// <summary>
+        /// 查看三天前的壁纸
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void threedayage_Click(object sender, RoutedEventArgs e)
         {
+            Frame.Navigate(typeof(DayPicturesPage), 3);
 
         }
 
+        /// <summary>
+        /// 查看更早的壁纸
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void other_Click(object sender, RoutedEventArgs e)
         {
-
+            showMorePicture.Begin();
         }
 
+        /// <summary>
+        /// 查看更早的壁纸 减少天数的图标按钮事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void minus_bar_Click(object sender, RoutedEventArgs e)
         {
+            int day = Int32.Parse(dayNumber.Text);
+            if (day > 0)
+            {
+                day--;
+                dayNumber.Text = day.ToString();
+            }
+        }
 
+        /// <summary>
+        /// 查看更早的壁纸，增加天数的图标按钮事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void plus_bar_Click(object sender, RoutedEventArgs e)
+        {
+            int day = Int32.Parse(dayNumber.Text);
+            day++;
+            dayNumber.Text = day.ToString();
+        }
+
+        /// <summary>
+        /// 前往查看自定义天数的壁纸
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void go_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(DayPicturesPage), Int32.Parse(dayNumber.Text));
+        }
+
+        private void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            if (e.Size.Width <= 500)
+            {
+                background.UriSource = new Uri(TodayPictureUri);
+            }
+            else
+            {
+                //background.UriSource = new Uri(TodayPictureUri_Big);
+            }
         }
     }
 }
